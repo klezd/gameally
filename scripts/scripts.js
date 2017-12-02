532dda568c2cd7d42f00cb1b99a8b416381fe6d2
Unknown <roxydao97@gmail.com>
2017-12-02 02:15:57 +0200

    change theme in header and 2 pages
    2 page: user page and edit page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,9 @@
 // header - icon of user info
 $(document).ready(function() {
-               var menu = TweenLite.to($(".menu"), 0.25, {opacity: 1, top: "10px", "z-index": 1});
+               var menu = TweenLite.to($(".menu"), 0.25, {opacity: 1, top: "10px", "z-index": 100});
                menu.reversed(true);
 
-               $("img#userInfoIcon").click(function() {
+               $("#userInfo").click(function() {
                    if(menu.reversed())
                    {
                        menu.play();
