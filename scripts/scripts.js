9d01bc1bc0266175197dad4b57cf426884841920
dothuan96 <dov.thuan96@gmail.com>
2017-12-04 15:36:23 +0200

    change sth

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,10 @@
+
 // header - icon of user info
 $(document).ready(function() {
-               var menu = TweenLite.to($(".menu"), 0.25, {opacity: 1, top: "10px", "z-index": 1});
+               var menu = TweenLite.to($(".menu"), 0.25, {opacity: 1, top: "10px", "z-index": 100});
                menu.reversed(true);
 
-               $("img#userInfoIcon").click(function() {
+               $("#userInfo").click(function() {
                    if(menu.reversed())
                    {
                        menu.play();
@@ -13,8 +14,9 @@ $(document).ready(function() {
                        menu.reverse();
                    }
                });
-           });
-
-// add sport to sport sportField
-
-addsport()
+});
+//signin in forgot password
+function showmore() {
+ document.getElementById("flip").classList.add("show-more");
+ document.getElementById("verify-btn").textContent = "Resend";
+}
