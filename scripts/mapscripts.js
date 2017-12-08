32b99a0010a9419d2937cbdf24c8a5199ef479fa
dothuan96 <dov.thuan96@gmail.com>
2017-12-08 17:16:38 +0200

    8/12

--- scripts/mapscripts.js
+++ scripts/mapscripts.js
@@ -11,6 +11,11 @@ function initAutocomplete() {
     zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
+  var map03 = new google.maps.Map(document.getElementById('map03'), { // i = 2
+    center: new google.maps.LatLng(51.508742,-0.120850),
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
   // Create the search box and link it to the UI element.
   var input = document.getElementsByClassName('searchbox');
 //  var searchBox = [];
@@ -126,7 +131,63 @@ function initAutocomplete() {
         map02.fitBounds(bounds);
       });
     }
+    else if (i==2) {
+     var searchBox03 = new google.maps.places.SearchBox(input[i]);
+
+     map03.controls[google.maps.ControlPosition.TOP_LEFT].push(input[i]);
+     // Bias the SearchBox results towards current map's viewport.
+     map03.addListener('bounds_changed', function() {
+       searchBox03.setBounds(map03.getBounds());
+     });
+     var markers = [];
+     searchBox03.addListener('places_changed', function() {
+       var places = searchBox03.getPlaces();
+
+       if (places.length == 0) {
+         return;
+       }
+
+       // Clear out the old markers.
+       markers.forEach(function(marker) {
+         marker.setMap(null);
+       });
+       markers = [];
+
+       // For each place, get the icon, name and location.
+       var bounds = new google.maps.LatLngBounds();
+       places.forEach(function(place) {
+         if (!place.geometry) {
+           console.log("Returned place contains no geometry");
+           return;
+         }
+         var icon = {
+           url: place.icon,
+           size: new google.maps.Size(71, 71),
+           origin: new google.maps.Point(0, 0),
+           anchor: new google.maps.Point(17, 34),
+           scaledSize: new google.maps.Size(25, 25)
+         };
+
+         // Create a marker for each place.
+         markers.push(new google.maps.Marker({
+           map: map,
+           icon: icon,
+           title: place.name,
+           position: place.geometry.location
+         }));
+
+         if (place.geometry.viewport) {
+           // Only geocodes have viewport.
+           bounds.union(place.geometry.viewport);
+         } else {
+           bounds.extend(place.geometry.location);
+         }
+       });
+       map03.fitBounds(bounds);
+     });
+   }
   }
+
 } //end func
 /* for 1 map search
 // map things go here ---- map for edit in profile
