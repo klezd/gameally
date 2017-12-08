ad124e6431e2d55c76dab29692efcfe43a5f8ea3
Unknown <roxydao97@gmail.com>
2017-12-08 13:30:18 +0200

    edit map on header for index pages, user pages
    done!

--- scripts/mapscripts_for-edit-page-only.js
+++ scripts/mapscripts_for-edit-page-only.js
@@ -1,12 +1,15 @@
-// map things go here ---- map for edit in profile
-function initAutocomplete() {
-
-  var map01 = new google.maps.Map(document.getElementById('map01'), { //i = 0
+function initAutocomplete01() {
+  var map = new google.maps.Map(document.getElementById('map'), { //edit profile i = 2
+    center: {lat: -33.8688, lng: 151.2195},
+    zoom: 13,
+    mapTypeId: 'roadmap',
+  });
+  var map01 = new google.maps.Map(document.getElementById('map01'), { //find match i = 0
     center: new google.maps.LatLng(51.508742,-0.120850),
     zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
-  var map02 = new google.maps.Map(document.getElementById('map02'), { // i = 1
+  var map02 = new google.maps.Map(document.getElementById('map02'), { //create match i = 1
     center: new google.maps.LatLng(51.508742,-0.120850),
     zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -16,7 +19,61 @@ function initAutocomplete() {
 //  var searchBox = [];
   for(i=0; i<input.length;i++) {
     //searchBox = new google.maps.places.SearchBox(input[i]);
-     if (i==0) {
+    if(i==2) {
+      var searchBox = new google.maps.places.SearchBox(input[i]);
+      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input[i]);
+      // Bias the SearchBox results towards current map's viewport.
+      map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+      });
+      var markers = [];
+      searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+          return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+          marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+          if (!place.geometry) {
+            console.log("Returned place contains no geometry");
+            return;
+          }
+          var icon = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+          };
+
+          // Create a marker for each place.
+          markers.push(new google.maps.Marker({
+            map: map,
+            icon: icon,
+            title: place.name,
+            position: place.geometry.location
+          }));
+
+          if (place.geometry.viewport) {
+            // Only geocodes have viewport.
+            bounds.union(place.geometry.viewport);
+          } else {
+            bounds.extend(place.geometry.location);
+          }
+        });
+        map.fitBounds(bounds);
+      });
+    }
+    else if (i==0) {
       var searchBox01 = new google.maps.places.SearchBox(input[i]);
 
       map01.controls[google.maps.ControlPosition.TOP_LEFT].push(input[i]);
@@ -128,77 +185,3 @@ function initAutocomplete() {
     }
   }
 } //end func
-/* for 1 map search
-// map things go here ---- map for edit in profile
-function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.8688, lng: 151.2195},
-    zoom: 13,
-    mapTypeId: 'roadmap',
-  });
-
-  // Create the search box and link it to the UI element.
-  var input = document.getElementsById('pac-input');
-
-  var searchBox = new google.maps.places.SearchBox(input);
-
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
-}
-
-
-*/
